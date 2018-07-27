04278352a3d9cc78b90b552fe4016c7bde37b822
jiadibo <jihad.strange@gmail.com>
2018-07-27 15:22:58 +0800

    third commit

--- app/src/main/java/com/example/jiadibo/fooddelivery/MainActivity.java
+++ app/src/main/java/com/example/jiadibo/fooddelivery/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.jiadibo.fooddelivery;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnSignIn;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/02_APompadourTextSample.ttf");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

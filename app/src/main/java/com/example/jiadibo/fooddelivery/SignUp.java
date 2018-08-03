11e77448280ee1a240ec3219eb4d85164e25519d
jiadibo <jihad.strange@gmail.com>
2018-08-03 23:17:09 +0800

    commit ni

--- app/src/main/java/com/example/jiadibo/fooddelivery/SignUp.java
+++ app/src/main/java/com/example/jiadibo/fooddelivery/SignUp.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.jiadibo.fooddelivery.Model.User;
@@ -16,53 +15,49 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
-public class SignIn extends AppCompatActivity {
-    EditText edtPhone,edtPassword;
-    Button btnSignIn;
-
+public class SignUp extends AppCompatActivity {
+    MaterialEditText edtPhone, edtName, edtPassword;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
 
-        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
-        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
-        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        edtName = (MaterialEditText) findViewById(R.id.edtName);
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText) findViewById(R.id.edtPhone);
 
-        //init database fb
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        //init db fbs
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference table_user = database.getReference("User");
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
                 mDialog.setMessage("Please waiting...");
                 mDialog.show();
 
                 table_user.addValueEventListener(new ValueEventListener() {
-
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        //check if user not exist in database
-                        if(dataSnapshot.child(edtPhone.getText().toString()).exists()) {
-                            //get user information
+                        if (dataSnapshot.child(edtPhone.getText().toString()).exists())
+                        {
                             mDialog.dismiss();
-                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
-                            if (user.getPassword().equals(edtPassword.getText().toString())) {
-                                Toast.makeText(SignIn.this, "Sign In successfully!", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(SignIn.this, "username or password wrong!", Toast.LENGTH_SHORT).show();
-                            }
+                            Toast.makeText(SignUp.this, "phone number already exist!", Toast.LENGTH_SHORT).show();
                         }
                         else
                         {
-                            Toast.makeText(SignIn.this, "User not exist!", Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                            User user = new User(edtName.getText().toString(),edtPassword.getText().toString());
+                            table_user.child(edtPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Sign up successfully !", Toast.LENGTH_SHORT).show();
+                            finish();
                         }
-
                     }
 
                     @Override
@@ -70,6 +65,7 @@ public class SignIn extends AppCompatActivity {
 
                     }
                 });
+
             }
         });
 
